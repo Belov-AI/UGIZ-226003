828188c1d96cf552ee56a013f8f3eaa4f15a5786
Belov-AI <Belov.Alexander@urfu.ru>
2024-06-13 15:41:40 +0500

    Yeld return

---
+++ Sequences/Examples/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sequences;
+
+namespace Examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sequence = new Sequence();
+
+            Console.WriteLine("Введите число членов полседовательности");
+            var n = int.Parse(Console.ReadLine());
+
+            var count = 0;
+
+            Console.WriteLine("Числа Фибоначчи");
+
+            foreach(var item in sequence.Fibonacci()) 
+            {
+                Console.Write($"{item} ");
+                count++;
+
+                if (count == n)
+                    break;
+            }
+
+            Console.WriteLine("\n\nФакторалы");
+
+            count = 0;
+
+            foreach(var item in sequence.Factorial())
+            {
+                Console.Write($"{item} ");
+                count++;
+
+                if (count == n)
+                    break;
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sequences/Sequences/Sequence.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sequences
+{
+    public class Sequence
+    {
+        public IEnumerable Fibonacci()
+        {
+            long a = 0;
+            long b = 1;
+
+            yield return a;
+            yield return b;
+
+            long c;
+
+            while (true)
+            {
+                c = a + b;
+                yield return c;
+
+                a = b;
+                b = c;
+            }
+        }
+
+        public IEnumerable Factorial()
+        {
+            long f = 1;
+            long n = 1;
+
+            while (true)
+            {
+                f = f * n;
+                yield return f;
+                n++;
+            }
+        }
+    }
+}
